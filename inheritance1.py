575c64fba1ce61ba662328ab9135e220498b29dc
Krutarth Patel <krutarthpatel146@gmail.com>
2023-06-20 16:11:48 +0530

    Practice

--- inheritance1.py
+++ inheritance1.py
@@ -27,3 +27,35 @@ class Dhrav(Father):
 d1 = Dhrav("Creta", "Dhrav_delux")
 d1.show_details()
 d1.show_property()
+
+
+
+num1 = 9
+
+
+if num1 % 2 == 0:
+    i = num1  # i = 9
+
+    while i < num1 * 3:   # 9 < 27
+        print(i,end=' ')
+        i+=2
+else:
+    i = num1  # i = 9
+
+    while i < num1 * 3:   # 9 < 27
+        print(i,end=' ')
+        i+=2
+
+
+str1 = '( ()) ((()()())) (()) ()'      
+str2 = ""
+l1 = []
+for i in str1.replace(' ',''):
+    # print(i)
+    str2 += i
+
+    if str2.count('(') == str2.count(')'):
+        l1.append(str2)
+        str2 = ""
+
+print(l1)  # ['(())', '((()()()))', '(())', '()']
